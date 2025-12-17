bfc3d3b15c37001a633fb0e2cb33edbc49f42f44
Anchu749 <jasbirverma441441@gmail.com>
2025-12-17 12:58:09 +0530

    Added new dashboard page

---
+++ Day_19_17.12/route.js
@@ -0,0 +1,50 @@
+const express= require('express');
+const myroute = express.Router();
+const multer= require('multer');
+const path = require('path');
+const Apoint_Object = require('./Controller/AppointmentController');
+const cont_obj = require('./Controller/ContactController');
+const user_obj = require('./Controller/AuthenticateController')
+
+
+
+// const  _____________ =multer.diskStorage({
+//     destination:'./public...path___________',
+//     filename:function(req,file,cd)
+//     {
+//         createBrotliCompress(null,Date.now()+path.extname(file.originalname))
+//     }
+// })
+// const emp_upload = multer({storage:____________________})
+
+
+
+myroute.get('/',(req,res)=>{
+    res.render('home')
+})
+
+myroute.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+myroute.post('/bookapointment',(req,res)=>{
+    Apoint_Object.Add_appointment(req,res)
+})
+
+myroute.use('/contact',(req,res)=>{
+    cont_obj.Enquire_Contact(req,res)
+})
+myroute.use('/plogin',(req,res)=>{
+    user_obj.checkAuth(req,res)
+})
+myroute.use('/paccount',(req,res)=>{
+    user_obj.create_user(req,res)
+})
+myroute.use('/Patient_Dashboard',(req,res)=>{
+    user_obj.Patients_dashboard(req,res)
+})
+
+
+
+
+module.exports=myroute;
