58c0e6d86a906b19ea5e6128f16d41a01b19552a
Anchu749 <jasbirverma441441@gmail.com>
2025-12-23 13:12:08 +0530

    Added new profile completing page

---
+++ Day_23_23.12/Controller/ContactController.js
@@ -0,0 +1,32 @@
+const contact_modal = require('../Modal/contact')
+class contact 
+{
+    Enquire_Contact(req,res)
+    {
+    if(req.method=='GET')
+        {
+            res.render('Contact')
+        }
+    else
+       {
+                    const data = {
+                    Name:req.body.name,
+                    Email:req.body.email,
+                    Subject:req.body.subject,
+                    Message:req.body.message,
+         
+                    
+            }
+            contact_modal.create(data,(err)=>{
+                if(err){
+                    res.render('Contact',{msg:'Something went wrong'})
+                }
+                else{
+                    res.render('Contact',{msg:"Your enquiry has been submitted successffuly"})
+                }
+            })
+       }
+    }
+}
+
+module.exports = new contact()
---
+++ Day_23_23.12/Modal/Appointment.js
@@ -0,0 +1,21 @@
+const db=require("../Databsae/myconnect")
+
+const Appointment= 
+{ 
+    create : (data, callback)=>
+    {
+    const q = `insert into appointment (name, email, phone, presefered_date , prefferr_time, address) values ('${data.Name}','${data.Email}','${data.Phone}','${data.Date}','${data.Time}','${data.Address}')`
+    db.query(q,callback)
+    },
+    delete : (data, callback)=>
+    {
+        const q =`delete from appointment where id =${data.id}`
+        db.query(q,callback)
+    },
+    list_appointment:(callback)=>{
+        const q= `select * from appointment`
+        db.query(q,callback)
+    }
+}
+
+module.exports= Appointment
